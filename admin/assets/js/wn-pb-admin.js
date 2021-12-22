5d78de960609253f8d6bc910de6dd29d9be594ad
Yaidier <yaidier.perez@gmail.com>
2021-12-22 13:27:48 +0100

    export feature | import feature | clone feature | css improvements

--- admin/assets/js/wn-pb-admin.js
+++ admin/assets/js/wn-pb-admin.js
@@ -9,18 +9,25 @@ jQuery(document).ready( function($) {
             const product_id    = $(this).parents('tr').children('td:first-child').text();
             const action        = $(this).attr('action');
     
-            console.log( 'new click' );
-    
             switch (action) {
-    
-                case 'add_new': manage_products_ajax_call('add_new_product');  
+
+                case 'clone_product':   manage_products_ajax_call( 'clone_product', product_id  );  
                     break;
     
-                case 'remove_product': manage_products_ajax_call( 'remove_product', product_id );  
+                case 'export_product':  manage_products_ajax_call( 'export_product', product_id  );  
                     break;
 
-                case 'edit_product': open_products_editor( product_id );  
+                case 'import_product':  import_product_get_file_data( product_id );  
                     break;
+    
+                case 'edit_product':    open_products_editor( product_id );  
+                    break;
+
+                case 'remove_product':  manage_products_ajax_call( 'remove_product', product_id );  
+                    break;
+
+                case 'add_new':         manage_products_ajax_call('add_new_product');  
+                    break;  
             
                 default:
                     break;
@@ -36,9 +43,28 @@ jQuery(document).ready( function($) {
         
     }
 
-    function manage_products_ajax_call( command, product_id ) {
+    function import_product_get_file_data( product_id ) {
 
-        console.log( product_id );
+        let input_file_field = '<input type="file" id="wn_pb_import_file" class="wn_pb_import_file">';
+        $('body').append( input_file_field );
+
+        $('.wn_pb_import_file').click();
+        $('.wn_pb_import_file').on('change', function(){
+
+            let file_to_read = this.files[0];
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                const raw_content = e.target.result;
+                const content = JSON.parse(raw_content); // parse json 
+
+                $('.wn_pb_import_file').remove();
+                manage_products_ajax_call( 'import_product', product_id, content )
+            };
+            reader.readAsText(file_to_read);
+        });
+    }
+
+    function manage_products_ajax_call( command, product_id, content = null ) {
 
         //Calling Ajax
         $.ajax({
@@ -50,6 +76,7 @@ jQuery(document).ready( function($) {
                 action: 'manage_products', 
                 command: command,
                 product_id: product_id,
+                content: content,
 
             },
             error: function( response ){
@@ -58,14 +85,39 @@ jQuery(document).ready( function($) {
             },
             success: function( response ) {
 
-                // Update the table
-                console.log( 'new response' );
-                update_element('.wn_pb_products_table', response);
-                alert_notification('Changes Saved');
+                switch (response['from']) {
+    
+                    case 'render_products_table': { 
+                        update_element('.wn_pb_products_table', response['data']); 
+                        alert_notification( response['status'], response['message'] );
+                    }
+                        break;
+                        
+                    case 'export_product': {
+                        download_product( response['data'] ); 
+                        alert_notification( response['status'], response['message'] );
+                    }
+                        break;
 
+                    case 'import_product': alert_notification( response['status'], response['message'] );
+                        break;
+    
+                    default: ;
+                        break;
+                }
             }
-
         });
+    }
+
+    function download_product( download_url ) {
+
+        var a = document.createElement('a');
+        a.href = download_url;
+        a.download = '';
+        document.body.append(a);
+        a.click();
+        a.remove();
+
     }
 
     function update_element( element, data ) {
@@ -92,7 +144,26 @@ jQuery(document).ready( function($) {
 
     }
 
-    function alert_notification( message ) {
+    function alert_notification( status = 'success',  message = '' ) {
+
+        $('.wn_pb_not').removeClass('wn_pb_not__success');
+        $('.wn_pb_not').removeClass('wn_pb_not__fail');
+
+        switch ( status ) {
+
+            case 'success': {
+                $('.wn_pb_not_content h4').text( 'SUCCESS' );
+                $('.wn_pb_not').addClass('wn_pb_not__success');
+            }
+                break;
+
+            case 'fail': {
+                $('.wn_pb_not_content h4').text( 'FAIL' );
+                $('.wn_pb_not').addClass('wn_pb_not__fail');
+            }
+                break;
+
+        }
 
         $('.wn_pb_not_content p').text( message );
         $('.wn_pb_not').addClass('wn_pb_not--active');
